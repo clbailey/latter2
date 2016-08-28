6b936c662ea1fd921c0083aa8262b923439486a6
Carter Bailey <carter.l.bailey@gmail.com>
2016-08-28 18:27:16 -0500

    posts, datetime, validation, etc. added

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :time_zone
+
+  has_many :connections, dependent: :destroy
+  has_many :posts, dependent: :destroy 
+
+end
